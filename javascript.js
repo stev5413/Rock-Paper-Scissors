6d829976d765d8cb2edcec7cc1220a7a1d425997
stev5413 <78006810+stev5413@users.noreply.github.com>
2025-02-26 11:23:48 -0600

    psuedocode first attempt

---
+++ javascript.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+/*
+Rock, Paper, Scissors
+
+First Draft Psuedocode
+1. Enter 1 for rock, 2 for paper, 3 for scissors
+2. function that randomly picks a number 1-3
+3. function that returns "rock, paper, or scissors"
+4. Repeat this 3 times
+5. compare results, rock > scissors, paper > rock, scissors > paper
+6. Add up score
+7. Return results 
+*/
+
